35af28f1b580bfed6a81eb15dd95528252bfe75e
Mauricio Gutierrez <developer.mauricio2310@gmail.com>
2022-02-07 15:32:29 +0000

    Backup 7 de feb 10:22 am

--- wp-content/themes/woodmart/header.php
+++ wp-content/themes/woodmart/header.php
@@ -9,6 +9,8 @@
 	<link rel="profile" href="http://gmpg.org/xfn/11">
 	<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
 
+	<meta name="google-site-verification" content="Ammoj7Y9GZ5Cmtx32wRkg-XcfHEB3gpnaHmvHzf5Fac" />
+	<!-- Global site tag (gtag) - Google Ads: 644710920 --> <amp-analytics type="gtag" data-credentials="include"> <script type="application/json"> { "vars": { "gtag_id": "AW-644710920", "config": { "AW-644710920": { "groups": "default" } } }, "triggers": { } } </script> </amp-analytics>
 	<?php wp_head(); ?>
 </head>
 
